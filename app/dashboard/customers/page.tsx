63c479551b4e89b2dd5ec75dad4ca33f9025a9f2
davidson.05@outlook.com <davidson.05@outlook.com>
2024-04-02 17:41:13 -0300

    add customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,27 @@
+import CustomersTable from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton, TableRowSkeleton } from "@/app/ui/skeletons";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+interface PageProps {
+  searchParams: {
+    query: string;
+  };
+}
+
+export default function Page({ searchParams }: PageProps) {
+
+  const query = searchParams.query || "";
+
+  return (
+    <div className="w-full">
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+       <CustomersTable query={query}/>
+      </Suspense> 
+    </div>
+  )
 }
--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,16 +1,16 @@
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
-import {
-  CustomersTableType,
-  FormattedCustomersTable,
-} from '@/app/lib/definitions';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
-export default async function CustomersTable({
-  customers,
-}: {
-  customers: FormattedCustomersTable[];
-}) {
+interface CustomersTableProps {
+  query: string;
+}
+
+export default async function CustomersTable({ query }: CustomersTableProps) {
+  
+  const customers = await fetchFilteredCustomers(query);
+
   return (
     <div className="w-full">
       <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
