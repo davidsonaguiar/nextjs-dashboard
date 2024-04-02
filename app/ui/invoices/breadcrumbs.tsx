321846857a260a1bc07b2a35412d1376e44b3499
davidson.05@outlook.com <davidson.05@outlook.com>
2024-04-02 17:21:22 -0300

    final

--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -8,11 +8,11 @@ interface Breadcrumb {
   active?: boolean;
 }
 
-export default function Breadcrumbs({
-  breadcrumbs,
-}: {
+interface BreadcrumbsProps {
   breadcrumbs: Breadcrumb[];
-}) {
+}
+
+export default function Breadcrumbs({ breadcrumbs }: BreadcrumbsProps) {
   return (
     <nav aria-label="Breadcrumb" className="mb-6 block">
       <ol className={clsx(lusitana.className, 'flex text-xl md:text-2xl')}>
